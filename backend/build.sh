42f9f5ab18b785fc01f86e552015e40772b7e5ed
Lemon <lemon@linux.do>
2024-10-07 23:10:14 +0800

    chore: 移除 build.sh 中的 NPM 镜像配置并更新 .env 文件版本号
    移除 build.sh 中的 NPM 镜像配置以简化构建过程，更新 .env 文件中的 VERSION，从 2.14.266 和 2.14.389 调整为 2.14.270 和 2.14.391。

--- backend/build.sh
+++ backend/build.sh
@@ -65,7 +65,6 @@ docker buildx inspect --bootstrap
 # 使用 Docker Buildx 构建镜像，同时标记为 latest 和 VERSION，支持多架构
 docker buildx build --no-cache \
   --platform linux/amd64,linux/arm64 \
-  --build-arg NPM_CONFIG_REGISTRY=https://registry.npmmirror.com \
   -t ${HUB_USER}/${HUB_REPO}:${VERSION} \
   -t ${HUB_USER}/${HUB_REPO} . \
   --push \
