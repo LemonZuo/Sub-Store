e4e8ba0ee2d88b17ff980e1fb3f2b004962ec91e
Lemon <lemon@linux.do>
2025-01-14 11:28:52 +0800

    chore: 注释掉 build.sh 中的 nvm 和 pnpm 命令，并修改 .env 文件中的 Dockerfile 路径配置。

--- backend/build.sh
+++ backend/build.sh
@@ -3,8 +3,8 @@
 set -e
 
 # 加载 nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
 # 记录构建开始时间
 start_time=$(date +%s)
@@ -51,10 +51,10 @@ fi
 # 从 .env 文件中导入环境变量
 export $(cat .env | sed 's/#.*//g' | xargs)
 
-nvm use 18
-pnpm install
-pnpm build
-pnpm bundle
+#nvm use 18
+#pnpm install
+#pnpm build
+#pnpm bundle
 
 # 使用环境变量中的用户名和密码尝试登录Docker Hub
 docker login -u="${HUB_USER}" -p="${HUB_PASS}"
